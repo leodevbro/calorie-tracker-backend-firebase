5027d3bc2864e16c8f0570df6e83e89479417c5b
Levan Katsadze <leodevbro@gmail.com>
2022-08-25 23:22:52 +0400

    add initial functions for auth flow

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,325 @@ import * as functions from "firebase-functions";
 // // https://firebase.google.com/docs/functions/typescript
 //
 
+import { FieldPath, WhereFilterOp } from "@google-cloud/firestore";
+
+import admin = require("firebase-admin");
+// eslint-disable-next-line max-len
+// import { serviceAccount } from "./tcomfybike-firebase-adminsdk-g1r6b-c21fc8dd72";
+
+interface IUserRoles {
+  manager: boolean;
+  user: boolean;
+}
+
+interface IFirestoreTimeStamp {
+  nanoseconds: number;
+  seconds: number;
+}
+
+interface IFirestoreGeoPoint {
+  latitude: number;
+  longitude: number;
+}
+
+type tyFirestoreDocField =
+  | null
+  | boolean
+  | string
+  | number
+  | IFirestoreTimeStamp
+  | IFirestoreGeoPoint
+  // as only for input to db [start]
+  // | FieldValue // for serverTimestamp()
+  // as only for input to db [end]
+  | tyFirestoreDocField[]
+  | { [key: string]: tyFirestoreDocField };
+
+export interface ISimpleDoc_withId {
+  [key: string]: tyFirestoreDocField;
+  id: string;
+}
+
+export type tySimpleDoc_withoutId = {
+  [key: string]: tyFirestoreDocField;
+} & {
+  id?: never;
+};
+
+admin
+  .initializeApp
+  //   {
+  //   credential: admin.credential.cert({
+  //     clientEmail: serviceAccount.client_email,
+  //     privateKey: serviceAccount.private_key,
+  //     projectId: serviceAccount.project_id,
+  //   }),
+  // }
+  ();
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+
 export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
+  functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
+
+  // new Promise((res, req) => {
+  //   res(0);
+  // }).then((x) => {
+  //   new Promise((resolve, reject) => {
+  //     resolve(0);
+  //   }).then((x) => {
+  //     //
+  //   });
+  // });
 });
+
+const firebaseDocIntoSimpleDoc = (
+  rawDoc: admin.firestore.QueryDocumentSnapshot<admin.firestore.DocumentData>,
+) => {
+  if (rawDoc.exists) {
+    const simpleData_withoutId: tySimpleDoc_withoutId = rawDoc.data();
+
+    const simpleData_withId: ISimpleDoc_withId = {
+      ...simpleData_withoutId,
+      id: rawDoc.id,
+    };
+
+    return simpleData_withId;
+  } else {
+    return null;
+  }
+};
+
+export const firebaseAdmin_getCollection_byQuery = async ({
+  collectionId,
+  whereQueryArr,
+}: {
+  collectionId: string;
+  whereQueryArr: [string | FieldPath, WhereFilterOp, tyFirestoreDocField][]; // .where(`members.${uid}.active`, "==", "sdfsdf")
+}) => {
+  const colRef = admin.firestore().collectionGroup(collectionId);
+
+  const len = whereQueryArr.length;
+
+  let whereRef:
+    | admin.firestore.CollectionGroup<admin.firestore.DocumentData>
+    | admin.firestore.Query<admin.firestore.DocumentData> = colRef;
+
+  for (let i = 0; i < len; i += 1) {
+    whereRef = whereRef.where(...whereQueryArr[i]);
+  }
+
+  const fDocs = await whereRef.get();
+
+  const myDocs = fDocs.docs.map((doc) => {
+    const simpleDoc = firebaseDocIntoSimpleDoc(doc);
+    return simpleDoc;
+  });
+
+  return myDocs;
+};
+
+export const deleteOneUser = functions.https.onCall(
+  async (data: { userId: string }, context) => {
+    const userId = data.userId;
+    console.log(typeof context);
+
+    const gotError = { v: false };
+
+    try {
+      // first in Auth:
+      await admin.auth().deleteUser(userId);
+    } catch (err) {
+      console.log(err);
+      gotError.v = true;
+    }
+
+    if (gotError.v) {
+      return null;
+    }
+
+    try {
+      // second in Firestore database:
+      await admin.firestore().doc(`/users/${userId}`).delete();
+
+      // await admin.auth().createUser({email: "koka@mymail.com", password: undefined});
+
+      // const specificBikesForRatingsByThisUser =
+      //   await firebaseAdmin_getCollection_byQuery({
+      //     collectionId: "bikes",
+      //     whereQueryArr: [[`active`, "==", userId]],
+      //   });
+
+      // TODO: delete the user data in ratings and rental days
+    } catch (err) {
+      console.log(err);
+      gotError.v = true;
+    }
+
+    if (gotError.v) {
+      return null;
+    }
+
+    return userId;
+  },
+);
+
+export const updateOneUser = functions.https.onCall(
+  async (
+    data: {
+      userId: string;
+      password?: string;
+
+      info: {
+        email?: string;
+        firstName?: string;
+        lastName?: string;
+        roles?: IUserRoles;
+      };
+    },
+    context,
+  ) => {
+    const actorId = context.auth?.uid || null;
+    const idOfUserToUpdate = data.userId;
+    const forMyself = actorId === idOfUserToUpdate;
+    console.log(typeof context);
+
+    const gotError = { v: false };
+
+    try {
+      // first in Auth:
+
+      // if (forMyself) {
+      //   const credential = admin.auth.EmailAuthProvider.credential(
+      //     firebase.auth().currentUser.email,
+      //     providedPassword
+      //   );
+      // }
+
+      const theOb = {
+        email: data.info.email,
+        password: data.password,
+      };
+
+      // remove all properties with undefined value !!!!!!!!!!!!!!! otherwise backend will consider them as null values
+      const theOb2 = JSON.parse(JSON.stringify(theOb));
+
+      if (data.info.email || data.password) {
+        await admin.auth().updateUser(data.userId, theOb2);
+      }
+    } catch (err) {
+      console.log(err);
+      gotError.v = true;
+    }
+
+    if (gotError.v) {
+      return null;
+    }
+
+    try {
+      // second in Firestore database:
+
+      const theOb = data.info;
+
+      if (Object.values(theOb).some((x) => x !== undefined)) {
+        // remove all properties with undefined value !!!!!!!!!!!!!!! otherwise backend will consider them as null values
+        const theOb2 = JSON.parse(JSON.stringify(theOb));
+
+        await admin
+          .firestore()
+          .doc(`/users/${idOfUserToUpdate}`)
+          .update(theOb2);
+
+        // await admin.auth().createUser({email: "koka@mymail.com", password: undefined});
+
+        // const specificBikesForRatingsByThisUser =
+        //   await firebaseAdmin_getCollection_byQuery({
+        //     collectionId: "bikes",
+        //     whereQueryArr: [[`active`, "==", userId]],
+        //   });
+
+        // TODO: delete the user data in ratings and rental days
+      }
+    } catch (err) {
+      console.log(err);
+      gotError.v = true;
+    }
+
+    if (gotError.v) {
+      return null;
+    }
+
+    return {
+      idOfUserToUpdate,
+      actorId,
+    };
+  },
+);
+
+export const createOneUser = functions.https.onCall(
+  async (
+    data: {
+      password: string;
+
+      info: {
+        email: string;
+        created: number; // milliseconds from the ERA
+        firstName: string;
+        lastName: string;
+        roles: IUserRoles;
+      };
+    },
+    context,
+  ) => {
+    console.log(typeof context);
+
+    const gotError = { v: false };
+    const newUserIdObj = { v: "" };
+
+    try {
+      // first in Auth:
+
+      const theOb = { email: data.info.email, password: data.password };
+
+      // remove all properties with undefined value !!!!!!!!!!!!!!! otherwise backend will consider them as null values
+      const theOb2 = JSON.parse(JSON.stringify(theOb));
+
+      const newUserRec = await admin.auth().createUser(theOb2);
+
+      newUserIdObj.v = newUserRec.uid;
+    } catch (err) {
+      console.log(err);
+      gotError.v = true;
+    }
+
+    if (gotError.v || !newUserIdObj.v) {
+      return null;
+    }
+
+    try {
+      // second in Firestore database:
+
+      const theOb = data.info;
+
+      // remove all properties with undefined value !!!!!!!!!!!!!!! otherwise backend will consider them as null values
+      const theOb2 = JSON.parse(JSON.stringify(theOb));
+
+      await admin.firestore().doc(`/users/${newUserIdObj.v}`).create(theOb2);
+    } catch (err) {
+      console.log(err);
+      functions.logger.error(">>>>>>>>", err, "<<<<<<<<<", {
+        structuredData: true,
+      });
+      gotError.v = true;
+    }
+
+    if (gotError.v) {
+      return null;
+    }
+
+    return newUserIdObj.v;
+  },
+);
